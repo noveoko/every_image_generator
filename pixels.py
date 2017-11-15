649fbef78469b14c3d029159646346000e21ce56
Marcin Kraszewski <visitwarsaw@gmail.com>
2017-11-15 23:37:08 +0100

    updated the file

--- pixels.py
+++ pixels.py
@@ -1,36 +1,38 @@
 from PIL import Image
 import numpy as np
 import random
+import os.path
 
 max_color = 100
-width = 10
-height = 10
-
+width = 2560
+height = 1440
 allimages = []
 
 def getcolor():
     color = random.choice([255,0])
-    color = [color for a in range(0,3)]
+    color = [color,color,color]
     return color
 
 def setcolor(d=3):
     colors = [getcolor() for a in range(0,width)]
     return colors
 
+allids = []
 def makeimage():
     rgbArray = [setcolor(a) for a in range(0,height)]
     picid = np.sum(rgbArray)
-
-    newimage = Image.new('RGB', (len(rgbArray[0]), len(rgbArray)))  # type, size
-    newimage.putdata([tuple(p) for row in rgbArray for p in row])
-
-    if picid not in allimages:
-        allimages.append(picid)
+    if picid not in allids:
+        newimage = Image.new('RGB', (len(rgbArray[0]), len(rgbArray)))  # type, size
+        newimage.putdata([tuple(p) for row in rgbArray for p in row])
         newimage.save("images/{}.png".format(picid))  # takes type from filename extension
-        print('just saved image {}'.format(picid))
+        allids.append(picid)
+        print('just saved image {}\ttotal attempts:{}'.format(picid, count))
     else:
         pass
 
 
+count = 0
 while len(allimages) < 1000:
+    count += 1
+    print(count)
     makeimage()
